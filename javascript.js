c1d7e3bf67e070b2f0a3685132b811c32085fbab
j0srisk <j0srisk@gmail.com>
2023-01-07 15:00:07 -0600

    Added getPlayerChoice and playRound Functions

--- javascript.js
+++ javascript.js
@@ -1,9 +1,45 @@
-console.log("Hello World");
+const CHOICES = ["ROCK", "PAPER", "SCISSORS"];
 
-const CHOICES = ["Rock", "Paper", "Scissors"];
+function getPlayerChoice(){
+    
+    let PlayerChoice = prompt("Enter your choice: ").toUpperCase();
 
-function getComputerChoice(){
-    return CHOICES[Math.floor(Math.random() * 3)];
+    console.log("Player: " + PlayerChoice);
+
+    return PlayerChoice;
 }
 
-console.log(getComputerChoice);
+function getComputerChoice(){
+
+    let ComputerChoice = CHOICES[Math.floor(Math.random() * 3)];
+
+    console.log("Computer: " + ComputerChoice);
+
+    return ComputerChoice;
+}
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return "Tie";
+    } else if (playerSelection === "ROCK"){
+        if (computerSelection === "SCISSORS"){
+            return "You Win! Rock beats Scissors";
+        } else if (computerSelection === "PAPER"){
+            return "You Lose! Paper beats Rock";
+        }
+    } else if (playerSelection === "PAPER"){
+        if (computerSelection === "ROCK"){
+            return "You Win! Paper beats Rock";
+        } else if (computerSelection === "SCISSORS"){
+            return "You Lose! Scissors beats Paper";
+        }
+    } else if (playerSelection === "SCISSORS"){
+        if (computerSelection === "PAPER"){
+            return "You Win! Scissors beats Paper";
+        } else if (computerSelection === "ROCK"){
+            return "You Lose! Rock beats Scissors";
+        }
+    }
+}
+
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
