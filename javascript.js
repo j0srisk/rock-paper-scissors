3d3c219e0d626e6f2bcc6a007df881baef229aad
j0srisk <j0srisk@gmail.com>
2023-01-07 15:12:09 -0600

    Added game function

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ function getPlayerChoice(){
     
     let PlayerChoice = prompt("Enter your choice: ").toUpperCase();
 
-    console.log("Player: " + PlayerChoice);
+    console.log("Player Choice: " + PlayerChoice);
 
     return PlayerChoice;
 }
@@ -13,33 +13,62 @@ function getComputerChoice(){
 
     let ComputerChoice = CHOICES[Math.floor(Math.random() * 3)];
 
-    console.log("Computer: " + ComputerChoice);
+    console.log("Computer Choice: " + ComputerChoice);
 
     return ComputerChoice;
 }
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
+        console.log("Tie")
         return "Tie";
     } else if (playerSelection === "ROCK"){
         if (computerSelection === "SCISSORS"){
-            return "You Win! Rock beats Scissors";
+            console.log("You Win! Rock beats Scissors")
+            return "Win";
         } else if (computerSelection === "PAPER"){
-            return "You Lose! Paper beats Rock";
+            console.log("You Lose! Paper beats Rock")
+            return "Lose";
         }
     } else if (playerSelection === "PAPER"){
         if (computerSelection === "ROCK"){
-            return "You Win! Paper beats Rock";
+            console.log("You Win! Paper beats Rock")
+            return "Win";
         } else if (computerSelection === "SCISSORS"){
-            return "You Lose! Scissors beats Paper";
+            console.log("You Lose! Scissors beats Paper")
+            return "Lose";
         }
     } else if (playerSelection === "SCISSORS"){
         if (computerSelection === "PAPER"){
-            return "You Win! Scissors beats Paper";
+            console.log("You Win! Scissors beats Paper")
+            return "Win";
         } else if (computerSelection === "ROCK"){
-            return "You Lose! Rock beats Scissors";
+            console.log("You Lose! Rock beats Scissors")
+            return "Lose";
         }
     }
 }
 
-console.log(playRound(getPlayerChoice(), getComputerChoice()));
+function game(){
+
+    let PlayerWins = 0;
+    let ComputerWins = 0;
+
+    for (let i = 0; i < 5; i++){
+        console.log("Game " + (i+1));
+        let result = playRound(getPlayerChoice(), getComputerChoice());
+
+        if (result === "Win"){
+            PlayerWins = PlayerWins + 1;
+        } else if (result === "Lose") {
+            ComputerWins = ComputerWins + 1;
+        }
+
+        console.log("")
+    }
+
+    console.log("Player Wins: " + PlayerWins);
+    console.log("Computer Wins: " + ComputerWins);
+}
+
+game();
